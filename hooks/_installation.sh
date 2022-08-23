da35b608384f66f396ac7279a7fb9aa544e2e671
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2022-08-23 09:29:49 +0900

    Add PGP verification on installation by `curl` (#6)
    * Add gpg verification
    
    * Bump all hooks to the latest version
    
    * Add 'Create release' workflow
    
    * Add echo
    
    Co-authored-by: Yevhen Fabizhevskyi <yevhen.fabizhevskyi@woven-planet.global>

--- hooks/_installation.sh
+++ hooks/_installation.sh
@@ -2,6 +2,28 @@
 set -eu
 if ! command -v ktlint &> /dev/null
 then
-    echo "Installing KTlint..."
-    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.45.1/ktlint && chmod a+x ktlint
+  echo "Installing ktlint..."
+  if ! command -v brew &> /dev/null
+  then
+    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.46.1/ktlint && chmod a+x ktlint
+    echo "Installation done"
+    echo "Verifying ktlint..."
+    curl -sS https://keybase.io/ktlint/pgp_keys.asc | gpg --import
+    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.46.1/ktlint.asc
+    if gpg --verify ktlint.asc ktlint ; then
+      echo "Verification passed"
+      rm -f ktlint.asc
+      mv ktlint /usr/local/bin/ktlint
+    else
+      echo -e "Verification failed"
+      echo "Uninstalling ktlint..."
+      rm -f ktlint.asc
+      rm -f ktlint
+      echo "klint uninstalled"
+      exit 1
+    fi
+  else
+    brew install ktlint
+    echo "Installation done"
+  fi
 fi
