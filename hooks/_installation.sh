f38b962167fc99d33c7f8ed5cc073f584ccd30e1
fabasoad <fabasoad@gmail.com>
2025-01-07 23:18:35 +0900

    Add logging.sh

--- hooks/_installation.sh
+++ hooks/_installation.sh
@@ -1,29 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env sh
 set -eu
-if ! command -v ktlint &> /dev/null
-then
-  echo "Installing ktlint..."
-  if ! command -v brew &> /dev/null
-  then
-    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.46.1/ktlint && chmod a+x ktlint
-    echo "Installation done"
-    echo "Verifying ktlint..."
-    curl -sS https://keybase.io/ktlint/pgp_keys.asc | gpg --import
-    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.46.1/ktlint.asc
-    if gpg --verify ktlint.asc ktlint ; then
-      echo "Verification passed"
-      rm -f ktlint.asc
-      mv ktlint /usr/local/bin/ktlint
-    else
-      echo -e "Verification failed"
-      echo "Uninstalling ktlint..."
-      rm -f ktlint.asc
-      rm -f ktlint
-      echo "klint uninstalled"
-      exit 1
-    fi
+
+SCRIPT_PATH=$(realpath "$0")
+SRC_DIR_PATH=$(dirname "$SCRIPT_PATH")
+LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
+
+. "${LIB_DIR_PATH}/logging.sh"
+
+install_ktlint() {
+  if command -v ktlint &> /dev/null; then
+    ktlint_path="$(which ktlint)"
   else
-    brew install ktlint
-    echo "Installation done"
+    log_info "Installing ktlint..."
+    if command -v brew &> /dev/null; then
+      brew install ktlint
+      log_info "Installation done"
+      ktlint_path="$(which ktlint)"
+    else
+      curl -sSLO https://github.com/pinterest/ktlint/releases/download/1.5.0/ktlint && chmod a+x ktlint
+      log_info "Installation done"
+      log_info "Verifying ktlint..."
+      curl -sS https://keybase.io/ktlint/pgp_keys.asc | gpg --import
+      curl -sSLO https://github.com/pinterest/ktlint/releases/download/1.5.0/ktlint.asc
+      if gpg --verify ktlint.asc ktlint ; then
+        log_info "Verification passed"
+        rm -f ktlint.asc
+        ktlint_path="$(pwd)/ktlint"
+      else
+        log_error "Verification failed"
+        log_info "Uninstalling ktlint..."
+        rm -f ktlint.asc
+        rm -f ktlint
+        log_info "klint uninstalled"
+        exit 1
+      fi
+    fi
   fi
-fi
+  echo "${ktlint_path}"
+}
--- hooks/ktlint.sh
+++ hooks/ktlint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-bash "${SCRIPT_DIR}"/_installation.sh
+. "${SCRIPT_DIR}"/_installation.sh
+
+ktlint_path=$(install_ktlint)
 
 i=1
 for arg in "$@"; do
@@ -10,11 +12,11 @@ for arg in "$@"; do
 done
 
 if [[ $i -gt 1 ]]; then
-    echo "Kotlin Format check with ktlint is running against specified files..."
-    echo "${ktlint_args[*]}"
-    ktlint "${ktlint_args[*]}"
+  echo "Kotlin Format check with ktlint is running against specified files..."
+  echo "${ktlint_args[*]}"
+  ${ktlint_path} "${ktlint_args[*]}"
 else
-    echo "Kotlin format check with ktlint is running..."
-    ktlint
+  echo "Kotlin format check with ktlint is running..."
+  ${ktlint_path}
 fi
 echo "Kotlin format check finished"
